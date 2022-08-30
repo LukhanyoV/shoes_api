9cf343cf41c486d7a4fcfb7305fb23cffb3c35c7
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-08-30 11:38:38 +0200

    api now working

--- api/shoes-api.js
+++ api/shoes-api.js
@@ -1,12 +1,105 @@
-const ShoesAPI = () => {
-    const test = (req, res) => {
-        res.json({
-            result: "success"
-        })
+const ShoesAPI = (shoesService) => {
+
+    const getAll = async (req, res) => {
+        try {
+            const data = await shoesService.getAll()
+            res.json({
+                status: "success",
+                data
+            })
+        } catch (error) {
+            res.json({
+                status: "error",
+                error: error.stack
+            })
+        }
+    }
+
+    const getByBrand = async (req, res) => {
+        try {
+            const {brandname} = req.params
+            const data = await shoesService.getByBrand(brandname)
+            res.json({
+                status: "success",
+                data
+            })
+        } catch (error) {
+            res.json({
+                status: "error",
+                error: error.stack
+            })
+        }
+    }
+
+    const getBySize = async (req, res) => {
+        try {
+            const {size} = req.params
+            const data = await shoesService.getBySize(size)
+            res.json({
+                status: "success",
+                data
+            })
+        } catch (error) {
+            res.json({
+                status: "error",
+                error: error.stack
+            })
+        }
+    }
+
+    const getByBrandAndSize = async (req, res) => {
+        try {
+            const {brandname, size} = req.params
+            const data = await shoesService.getByBrandAndSize(brandname, size)
+            res.json({
+                status: "success",
+                data
+            })
+        } catch (error) {
+            res.json({
+                status: "error",
+                error: error.stack
+            })
+        }
+    }
+
+    const buyShoe = async (req, res) => {
+        try {
+            const {id} = req.params
+            const shoe = await shoesService.getById(id)
+            await shoesService.buyShoe(shoe)
+            res.json({
+                status: "success"
+            })
+        } catch (error) {
+            res.json({
+                status: "error",
+                error: error.stack
+            })
+        }
+    }
+
+    const addShoe = async (req, res) => {
+        try {
+            await shoesService.addShoe(req.body)
+            res.json({
+                status: "success"
+            })
+        } catch (error) {
+            res.json({
+                status: "error",
+                error: error.stack
+            })
+        }
     }
 
     return {
-        test
+        getAll,
+        getByBrand,
+        getBySize,
+        getByBrandAndSize,
+        buyShoe,
+        addShoe
     }
 }
 
