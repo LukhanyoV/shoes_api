2e4fc84458c937daeb0c5dbf5c53371624b20e98
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-08-30 11:38:19 +0200

    created factory function for shoes services

---
+++ public/js/shoesService.js
@@ -0,0 +1,35 @@
+const ShoesFunctions = () => {
+    const getAll = async () => {
+        const {data} = await axios.get(`/api/shoes`)
+        return data.data
+    }
+
+    const getByBrand = async (brand) => {
+        const {data} = await axios.get(`/api/shoes/brand/${brand}`)
+        return data.data
+    }
+
+    const getBySize = async (size) => {
+        const {data} = await axios.get(`/api/shoes/size/${size}`)
+        return data.data
+    }
+
+    const getByBrandAndSize = async (brand, size) => {
+        const {data} = await axios.get(`/api/shoes/brand/${brand}/size/${size}`)
+        return data.data
+    }
+
+    const addShoe = async (body) => {
+        console.log(body)
+        const {data} = await axios.post(`/api/shoes`, body)
+        return data.status
+    }
+
+    return {
+        getAll,
+        getByBrand,
+        getBySize,
+        getByBrandAndSize,
+        addShoe
+    }
+}
